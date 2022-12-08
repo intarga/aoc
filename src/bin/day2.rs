8c74942d22f77843cdfb349384a9cfc9fac062fb
Ingrid <git@ingrids.email>
2022-12-08 23:00:32 +0100

    solve day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,8 +3,6 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("input/day2").expect("failed to read input file");
 
-    // println!("{}", contents);
-
     let total_score: u32 = contents
         .lines()
         .map(|strategy| match strategy {
@@ -21,27 +19,23 @@ fn main() {
         })
         .sum();
 
-    // let scores = contents
-    //     .lines()
-    //     .map(|strategy| match strategy {
-    //         "A X" => 4,
-    //         "A Y" => 8,
-    //         "A Z" => 3,
-    //         "B X" => 1,
-    //         "B Y" => 5,
-    //         "B Z" => 9,
-    //         "C X" => 7,
-    //         "C Y" => 2,
-    //         "C Z" => 6,
-    //         _ => panic!("unrecognised strategy"),
-    //     })
-    //     .zip(contents.lines());
-
-    // for line in scores {
-    //     println!("strat: {}, score: {}", line.1, line.0)
-    // }
-
     println!("part1: {}", total_score);
 
-    // println!("part2: {}",);
+    let total_score: u32 = contents
+        .lines()
+        .map(|strategy| match strategy {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
+            _ => panic!("unrecognised strategy"),
+        })
+        .sum();
+
+    println!("part2: {}", total_score);
 }
