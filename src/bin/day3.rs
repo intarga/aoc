d3cf6f9bae3b941c20122c82562101a3374b0cf5
Ingrid <git@ingrids.email>
2022-12-11 20:57:04 +0100

    solve day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -20,10 +20,20 @@ fn rucksack_to_match_priority(rucksack: &str) -> Option<u32> {
     None
 }
 
+fn three_rucksacks_to_match_priority(rucksacks: &[&str]) -> Option<u32> {
+    assert!(rucksacks.len() == 3);
+
+    for item in rucksacks[0].chars() {
+        if rucksacks[1].contains(item) && rucksacks[2].contains(item) {
+            return item_to_priority(item);
+        }
+    }
+
+    None
+}
+
 fn main() {
     let contents = fs::read_to_string("input/day3").expect("failed to read input file");
-
-    // println!("{}", contents);
 
     let priority_sum: u32 = contents
         .lines()
@@ -31,5 +41,15 @@ fn main() {
         .sum();
 
     println!("part1: {}", priority_sum);
-    // println!("part2: {}",);
+
+    let priority_sum: u32 = contents
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|rucksacks| {
+            three_rucksacks_to_match_priority(rucksacks).expect("failed to find duplicate")
+        })
+        .sum();
+
+    println!("part2: {}", priority_sum);
 }
