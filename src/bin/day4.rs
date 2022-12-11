9c4cd8186c18e909ecb7b8521d78cbbdb5084fcf
Ingrid <git@ingrids.email>
2022-12-11 22:01:47 +0100

    solve day 4 part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,21 +1,43 @@
 use std::fs;
 
-fn pairs_to_encapsulation(pairs: &str) -> u32 {
+fn pairs_to_endpoints(pairs: &str) -> [u32; 4] {
     let split = pairs.split(&[',', '-'][..]);
 
-    let endpoints: Vec<u32> = split
+    split
         .map(|point_str| point_str.parse::<u32>().expect("failed to parse int"))
-        .collect();
+        .collect::<Vec<u32>>()
+        .try_into()
+        .expect("parsed wrong number of ints")
+}
 
+fn endpoints_to_encapsulation(endpoints: [u32; 4]) -> u32 {
     (endpoints[0] <= endpoints[2] && endpoints[1] >= endpoints[3]
         || endpoints[2] <= endpoints[0] && endpoints[3] >= endpoints[1]) as u32
+}
+
+fn endpoints_to_overlap(endpoints: [u32; 4]) -> u32 {
+    (endpoints[0] <= endpoints[2] && endpoints[1] >= endpoints[2]
+        || endpoints[0] <= endpoints[3] && endpoints[1] >= endpoints[3]
+        || endpoints[2] <= endpoints[0] && endpoints[3] >= endpoints[0]
+        || endpoints[2] <= endpoints[1] && endpoints[3] >= endpoints[1]) as u32
 }
 
 fn main() {
     let contents = fs::read_to_string("input/day4").expect("failed to read input file");
 
-    let num_encapsulations: u32 = contents.lines().map(pairs_to_encapsulation).sum();
+    let num_encapsulations: u32 = contents
+        .lines()
+        .map(pairs_to_endpoints)
+        .map(endpoints_to_encapsulation)
+        .sum();
 
     println!("part1: {}", num_encapsulations);
-    // println!("part2: {}",);
+
+    let num_overlaps: u32 = contents
+        .lines()
+        .map(pairs_to_endpoints)
+        .map(endpoints_to_overlap)
+        .sum();
+
+    println!("part2: {}", num_overlaps);
 }
