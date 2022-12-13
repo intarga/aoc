ec4f2a44417e8ff97aea0358cdffdcc422650054
Ingrid Rebecca Abraham <ingridra@met.no>
2022-12-13 14:23:30 +0100

    solve day 6 part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn is_start_of_packet(pair: &(&[char], isize)) -> bool {
+fn is_marker(pair: &(&[char], isize)) -> bool {
     for i in 0..(pair.0.len() - 1) {
         for j in (i + 1)..(pair.0.len()) {
             if pair.0[i] == pair.0[j] {
@@ -12,20 +12,27 @@ fn is_start_of_packet(pair: &(&[char], isize)) -> bool {
     true
 }
 
+fn find_marker(input: &Vec<char>, length: usize) -> usize {
+    input
+        .windows(length)
+        .zip(0..)
+        .find(is_marker)
+        .expect("no start of packet found")
+        .1 as usize
+        + length
+}
+
 fn main() {
     let contents = fs::read_to_string("input/day6")
         .expect("failed to read input file")
         .chars()
         .collect::<Vec<char>>();
 
-    let start_of_packet = contents
-        .windows(4)
-        .zip(0..)
-        .find(is_start_of_packet)
-        .expect("no start of packet found")
-        .1
-        + 4;
+    let start_of_packet = find_marker(&contents, 4);
 
     println!("part1: {}", start_of_packet);
-    // println!("part2: {}",);
+
+    let start_of_message = find_marker(&contents, 14);
+
+    println!("part2: {}", start_of_message);
 }
