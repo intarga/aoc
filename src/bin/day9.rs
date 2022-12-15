de9326f7d522f5208e34f7fa8b13c458b3af825d
Ingrid <git@ingrids.email>
2022-12-15 19:43:57 +0100

    solve day 9 part 2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,38 +1,45 @@
 use std::collections::HashSet;
 use std::fs;
 
-fn main() {
-    let contents = fs::read_to_string("input/day9").expect("failed to read input file");
+fn track_tail_visits(input: &String, rope_length: usize) -> usize {
+    let mut rope: Vec<(i32, i32)> = std::iter::repeat((0, 0)).take(rope_length).collect();
 
-    let mut head: (i32, i32) = (0, 0);
-    let mut tail: (i32, i32) = (0, 0);
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
-    let mut visited: HashSet<(i32, i32)> = HashSet::from([tail]);
-
-    for (dir, count) in contents
+    for (dir, count) in input
         .lines()
         .map(|line| line.split_once(' ').unwrap())
         .map(|(dir, count)| (dir, count.parse::<u32>().unwrap()))
     {
         for _ in 0..count {
-            let new_head = match dir {
-                "U" => (head.0, head.1 + 1),
-                "D" => (head.0, head.1 - 1),
-                "L" => (head.0 - 1, head.1),
-                "R" => (head.0 + 1, head.1),
+            rope[0] = match dir {
+                "U" => (rope[0].0, rope[0].1 + 1),
+                "D" => (rope[0].0, rope[0].1 - 1),
+                "L" => (rope[0].0 - 1, rope[0].1),
+                "R" => (rope[0].0 + 1, rope[0].1),
                 _ => panic!("unrecognised direction"),
             };
 
-            if std::cmp::max((new_head.0 - tail.0).abs(), (new_head.1 - tail.1).abs()) >= 2 {
-                tail = head;
-                visited.insert(tail);
+            for i in 1..rope_length {
+                let hdiff = rope[i - 1].0 - rope[i].0;
+                let vdiff = rope[i - 1].1 - rope[i].1;
+
+                if std::cmp::max(hdiff.abs(), vdiff.abs()) >= 2 {
+                    rope[i].0 += hdiff.signum();
+                    rope[i].1 += vdiff.signum();
+                }
             }
-            head = new_head;
+            visited.insert(*rope.last().unwrap());
         }
     }
 
-    let num_visited = visited.len();
+    visited.len()
+}
 
-    println!("part1: {}", num_visited);
-    // println!("part2: {}",);
+fn main() {
+    let contents = fs::read_to_string("input/day9").expect("failed to read input file");
+
+    println!("part1: {}", track_tail_visits(&contents, 2));
+
+    println!("part2: {}", track_tail_visits(&contents, 10));
 }
