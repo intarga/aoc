a0a73a131b9381ad975ba0760fc9e49b09cc19cf
Ingrid <git@ingrids.email>
2022-12-15 20:38:32 +0100

    solve day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -4,8 +4,9 @@ fn main() {
     let contents = fs::read_to_string("input/day10").expect("failed to read input file");
 
     let mut cycle = 1;
-    let mut x = 1;
+    let mut x: i32 = 1;
     let mut signal_sum = 0;
+    let mut crt: Vec<char> = Vec::new();
 
     for instruction in contents
         .lines()
@@ -16,6 +17,17 @@ fn main() {
             signal_sum += cycle * x;
         }
 
+        let crt_x = (cycle - 1) % 40;
+        if x - 1 <= crt_x && x + 1 >= crt_x {
+            crt.push('#');
+        } else {
+            crt.push('.');
+        }
+
+        if cycle % 40 == 0 {
+            crt.push('\n');
+        }
+
         match instruction {
             "noop" | "addx" => (),
             _ => x += instruction.parse::<i32>().unwrap(),
@@ -24,5 +36,5 @@ fn main() {
     }
 
     println!("part1: {}", signal_sum);
-    // println!("part2: {}",);
+    println!("part2: \n{}", crt.iter().collect::<String>());
 }
